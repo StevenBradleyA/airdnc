e643731befd021149824841bc9a2b935ffebea86
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-01-22 13:08:38 -0800

    router index fix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
+
+
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
@@ -17,12 +20,6 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-// ...
-
-// ...
-
-router.use('/api', apiRouter);
-// ...
 
 
 
