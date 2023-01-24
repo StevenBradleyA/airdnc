13a5bdd182de19b258605f305c0048b95bb135ca
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-01-24 13:57:03 -0800

    added seed data for Bookings

--- backend/db/seeders/20230124200125-demo-booking.js
+++ backend/db/seeders/20230124200125-demo-booking.js
@@ -1,25 +1,56 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    options.tableName = "Bookings";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          spotId: 1,
+          userId: 1,
+          startDate: "2021-11-19",
+          endDate: "2021-11-20",
+        },
+        {
+          spotId: 2,
+          userId: 2,
+          startDate: "2021-11-19",
+          endDate: "2021-11-20",
+        },
+        {
+          spotId: 3,
+          userId: 3,
+          startDate: "2021-11-19",
+          endDate: "2021-11-20",
+        },
+        {
+          spotId: 4,
+          userId: 4,
+          startDate: "2021-11-19",
+          endDate: "2021-11-20",
+        },
+        {
+          spotId: 5,
+          userId: 5,
+          startDate: "2021-11-19",
+          endDate: "2021-11-20",
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Bookings";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: { [Op.in]: [1, 2, 3, 4, 5] },
+      },
+      {}
+    );
+  },
 };
--- backend/db/seeders/20230124200149-demo-review.js
+++ backend/db/seeders/20230124200149-demo-review.js
@@ -3,15 +3,43 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    options.tableName = "Reviews";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          spotId: 1,
+          userId: 1,
+          review: "wow, this place was pog",
+          stars: 5,
+        },
+        {
+          spotId: 2,
+          userId: 2,
+          review: "wow, this place was pog",
+          stars: 5,
+        },
+        {
+          spotId: 3,
+          userId: 3,
+          review: "wow, this place was pog",
+          stars: 5,
+        },
+        {
+          spotId: 4,
+          userId: 4,
+          review: "wow, this place was pog",
+          stars: 5,
+        },
+        {
+          spotId: 5,
+          userId: 5,
+          review: "wow, this place was pog",
+          stars: 5,
+        },
+      ],
+      {}
+    );
   },
 
   async down (queryInterface, Sequelize) {
