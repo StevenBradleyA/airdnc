f732e9815fc11b297db7a6e3f9e450e92ac5fbcd
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-01-24 08:32:55 -0800

    ReviewImage model  association added

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+
+      // Review relationship to ReviewImages
+      Review.hasMany(models.ReviewImage, {foreignKey: 'reviewId'})
+
     }
   }
   Review.init({
--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      
+      // ReviewImage relationship to Review
+      ReviewImage.belongsTo(models.Review, { foreignKey: "reviewId" });
+
     }
   }
   ReviewImage.init({
