883922e91fe26bd9a45c620e6f8c2663399ce75c
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-01-24 10:30:25 -0800

    spots api route setup

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,15 @@
+// backend/routes/api/spots.js
+const express = require("express");
+const router = express.Router();
+
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Spot } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+
+
+
+module.exports = router;
