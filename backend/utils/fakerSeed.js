a0c663fc67eddd821af4f8c493d5d2950ecd7667
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-01-24 15:52:21 -0800

    faker seed addition

--- backend/utils/fakerSeed.js
+++ backend/utils/fakerSeed.js
@@ -1,21 +1,21 @@
-const { faker, GitModule } = require("@faker-js/faker");
-const bcrypt = require("bcryptjs");
+// const { faker, GitModule } = require("@faker-js/faker");
+// const bcrypt = require("bcryptjs");
 
-//simple random number generator
-const rNum = (num) => Math.floor(Math.random() * Math.floor(num) + 1);
+// //simple random number generator
+// const rNum = (num) => Math.floor(Math.random() * Math.floor(num) + 1);
 
-const seedUsers = (num) => {
-  let users = new Array(num).fill("");
-  for (const i in users) {
-    users[i] = {
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      hashedPassword: bcrypt.hashSync(faker.internet.password()),
-    };
-  }
-  return users;
-};
+// const seedUsers = (num) => {
+//   let users = new Array(num).fill("");
+//   for (const i in users) {
+//     users[i] = {
+//       username: faker.internet.userName(),
+//       email: faker.internet.email(),
+//       hashedPassword: bcrypt.hashSync(faker.internet.password()),
+//     };
+//   }
+//   return users;
+// };
 
-module.exports = {
-  seedUsers,
-};
+// module.exports = {
+//   seedUsers,
+// };
