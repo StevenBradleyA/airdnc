26a40ad31845ab164dea15cb3bf9642e792d20b6
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-01-28 23:18:01 -0800

    added delete a booking endpoint

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -13,6 +13,7 @@ const {
 } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+const { ResultWithContext } = require("express-validator/src/chain");
 
 //* GET /current
 // Return all the bookings that the current user has made.
@@ -65,6 +66,53 @@ router.get("/current", requireAuth, async (req, res, next) => {
   }
 
   return res.json(bookingData);
+});
+
+//! DELETE /:bookingId
+router.delete("/:id", requireAuth, async (req, res, next) => {
+  const deleteBooking = await Booking.findByPk(req.params.id);
+
+  if (!deleteBooking) {
+    return res.status(404).json({
+      message: "Booking couldn't be found",
+      statusCode: 404,
+    });
+  }
+  const spot = await Spot.findOne({
+    where: {
+      ownerId: req.user.id,
+    },
+  });
+  const booking = await Booking.findOne({
+    where: {
+      userId: req.user.id,
+    },
+  });
+
+  if (
+    (spot && spot.ownerId === req.user.id) ||
+    (booking && booking.userId === req.user.id)
+  ) {
+    deleteBooking.destroy();
+    return res.json({
+      message: "Successfully deleted",
+      statusCode: 200,
+    });
+  }
+  if (
+    (spot && spot.ownerId === req.user.id) ||
+    (booking && booking.userId === req.user.id)
+  ) {
+    return res.status(403).json({
+      message: "Forbidden",
+      statusCode: 403,
+    });
+  } else {
+    return res.status(403).json({
+      message: "Bookings that have been started can't be deleted",
+      statusCode: 403,
+    });
+  }
 });
 
 module.exports = router;
