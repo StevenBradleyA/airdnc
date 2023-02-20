8b7069994a723cf817ef48d4929bc7b2a4e305f2
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-02-20 14:06:42 -0800

    sign up authorization refactor

--- backend/app.js
+++ backend/app.js
@@ -43,23 +43,51 @@ app.use(
 
 app.use(routes); // Connect all the routes
 
+// app.use((_req, _res, next) => {
+//   const err = new Error("The requested resource couldn't be found.");
+//   err.title = "Resource Not Found";
+//   err.errors = ["The requested resource couldn't be found."];
+//   err.status = 404;
+//   next(err);
+// });
+
+
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
   err.title = "Resource Not Found";
-  err.errors = ["The requested resource couldn't be found."];
+  err.errors = { message: "The requested resource couldn't be found." };
   err.status = 404;
   next(err);
 });
 
+
 // Process sequelize errors
+// app.use((err, _req, _res, next) => {
+//   // check if error is a Sequelize error:
+//   if (err instanceof ValidationError) {
+//     err.errors = err.errors.map((e) => e.message);
+//     err.title = "Validation error";
+//   }
+//   next(err);
+// });
+
+
+
 app.use((err, _req, _res, next) => {
   // check if error is a Sequelize error:
   if (err instanceof ValidationError) {
-    err.errors = err.errors.map((e) => e.message);
-    err.title = "Validation error";
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
   }
   next(err);
 });
+
+
+
 
 // Error formatter
 app.use((err, _req, res, _next) => {
