155fea64511c18f702c2c2c481271477dd19cdac
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-03-13 08:49:37 -0700

    read spots action created

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,8 +2,26 @@ import { csrfFetch } from "./csrf";
 
 const READ_SPOTS = "spots/READ_SPOTS"
 
+const readSpots = (allSpotData)=> ({
+    type: READ_SPOTS,
+    payload: allSpotData
+})
+
+export const readAllSpotsAction = () => async (dispatch) => {
+    const response = await csrfFetch("/api/spots");
+    const data = await response.json()
+    console.log(data)
+    dispatch(readSpots(data))
 
 
+}
+
+// export const restoreUser = () => async (dispatch) => {
+//     const response = await csrfFetch("/api/session");
+//     const data = await response.json();
+//     dispatch(setUser(data.user));
+//     return response;
+//   };
 
 const initialState = {};
 
