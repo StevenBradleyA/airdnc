8278435fc32138aafa1524aa56108050d88bd7bf
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-03-17 12:06:32 -0700

    refactored log out button

---
+++ frontend/src/components/Navigation/Logout.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+
+
+function LogOutButton({ user, name }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+
+
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const closeMenu = () => setShowMenu(false);
+
+  const handleLogoutClick = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+    closeMenu();
+  };
+
+
+  return (
+      <div className="log-out-button" ref={ulRef}>
+        {user && <button onClick={handleLogoutClick}>{name}</button>}
+      </div>
+  );
+}
+
+export default LogOutButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,14 +5,17 @@ import ProfileButton from "./ProfileButton";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCouch, faBurger } from "@fortawesome/free-solid-svg-icons";
+
 import "./Navigation.css";
+import LogOutButton from "./Logout";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
   const [openMenu, setOpenMenu] = useState(false);
-  const history = useHistory()
+  const history = useHistory();
 
   const handleMenuClick = (e) => {
     e.preventDefault();
@@ -20,12 +23,13 @@ function Navigation({ isLoaded }) {
   };
   const handleCreateClick = (e) => {
     e.preventDefault();
-    history.push('/spots/new');
+    history.push("/spots/new");
   };
   const handleManageClick = (e) => {
     e.preventDefault();
-    history.push('/spots/current');
+    history.push("/spots/current");
   };
+
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
@@ -74,20 +78,25 @@ function Navigation({ isLoaded }) {
         />
         {openMenu && (
           <div className="menu-dropdown">
-              <h3>{`Hello, ${sessionUser.firstName} ${sessionUser.email}`}</h3>
-              <button className="your-profile-button">Your Profile</button>
             {sessionUser && (
-            <div className="manage-spot-button"> 
-              <button onClick={handleManageClick}>Manage Spots</button>
-           </div>
+              <h3>{`Hello, ${sessionUser.firstName} ${sessionUser.email}`}</h3>
             )}
-              <button>log out</button>
+            {sessionUser && (
+              <button className="your-profile-button">Your Profile</button>
+            )}
+            {sessionUser && (
+              <div className="manage-spot-button">
+                <button onClick={handleManageClick}>Manage Spots</button>
+              </div>
+            )}
+            {sessionUser && (
+              <LogOutButton user={sessionUser} name={`Log Out`} />
+            )}
             <div>{isLoaded && sessionLinks}</div>
           </div>
         )}
-        </div>
+      </div>
     </div>
-
   );
 }
 
